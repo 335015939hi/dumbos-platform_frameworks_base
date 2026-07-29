Katya-Incorporated/Frameworks_base

add test for SELinux flags-based per-app hardening features
Currently covers restriction of dynamic code loading via memory/storage and blocking of ptrace
access.

Testing is performed on three kinds of HardeningTestApp: targetSdk 27, current targetSdk and
preinstalled current targetSdk app (the latter is included on debuggable builds).

Preinstalled app is tested separately to make sure that restrictions can't be weakened for it,
regardless of GosPackageState values.

To run the test, execute `atest HardeningTest`.

tests/HardeningTest/HardeningTestApp/jni/hardening_test.cpp
@@ -1,0 +1,133 @@
+#include <cstdlib>
+
+#include <android/sharedmem.h>
+#undef NDEBUG // needed to enable assertions
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <jni.h>
+#include <linux/ashmem.h>
+#include <sys/mman.h>
+#include <sys/ptrace.h>
+#include <unistd.h>
+
+static jint get_errno() {
+    int r = errno;
+    assert(r > 0);
+    return (jint) r;
+}
+
+static constexpr size_t DEFAULT_BUF_LEN = 16 * 1024 + 10;
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execmem(JNIEnv *env, jclass cls) {
+    size_t len = DEFAULT_BUF_LEN;
+    void *addr = mmap(nullptr, len, PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (addr != MAP_FAILED) {
+        munmap(addr, len);
+        return 0;
+    }
+    return get_errno();
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execmod(JNIEnv *env, jclass cls, jint fd) {
+    char buf[DEFAULT_BUF_LEN];
+    assert((ssize_t) sizeof(buf) == write(fd, buf, sizeof(buf)));
+
+    void *addr = mmap(nullptr, sizeof(buf), PROT_WRITE, MAP_PRIVATE, fd, 0);
+    close(fd);
+    assert(addr != nullptr);
+
+    // modify the file mapping
+    (* ((char*) addr))++;
+
+    jint r = 0;
+    if (mprotect(addr, sizeof(buf), PROT_EXEC) != 0) {
+        r = get_errno();
+    }
+
+    munmap(addr, sizeof(buf));
+    return r;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_exec_1app_1data_1file(JNIEnv *env, jclass cls, jint fd) {
+    const char* argv[] = { "a", nullptr };
+    const char* envp[] = { "b", nullptr };
+
+    fexecve(fd, (char *const *) argv, (char *const *) envp);
+    close(fd);
+    return get_errno();
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_exec_1appdomain_1tmpfs(JNIEnv *env, jclass cls) {
+    int fd = memfd_create("hardeningtest_memfd", 0);
+    assert(fd >= 0);
+
+    size_t len = DEFAULT_BUF_LEN;
+
+    void *addr = mmap(nullptr, len, PROT_EXEC, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    if (addr == MAP_FAILED) {
+        return get_errno();
+    }
+
+    munmap(addr, len);
+    return 0;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_ptrace(JNIEnv *env, jclass cls, jint pid) {
+    pid_t fork_res = fork();
+
+    if (fork_res == 0) {
+        // child
+        // not necessary and racy
+        // ptrace(PTRACE_TRACEME, 0, nullptr, 0);
+    } else {
+        assert(fork_res > 0);
+        pid_t child_pid = fork_res;
+        long r = ptrace(PTRACE_ATTACH, child_pid, nullptr, 0);
+        if (r == -1) {
+            return get_errno();
+        }
+        ptrace(PTRACE_DETACH, child_pid, nullptr, 0);
+    }
+    return 0;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execute_1ashmem(JNIEnv *env, jclass cls) {
+    // Based on ashmem_create_region() from system/core/libcutils/ashmem-dev.cpp
+    // Direct use of that function would return memfd instead of ashmem fd
+
+    int fd = open("/dev/ashmem", O_RDWR);
+    if (fd < 0) {
+        return get_errno();
+    }
+
+    size_t size = DEFAULT_BUF_LEN;
+
+    assert(ioctl(fd, ASHMEM_SET_SIZE, size) == 0);
+
+    void *addr = mmap(nullptr, size, PROT_EXEC, MAP_PRIVATE, fd, 0);
+    close(fd);
+    if (addr == MAP_FAILED) {
+        return get_errno();
+    }
+    munmap(addr, size);
+    return 0;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execute_1ashmem_1libcutils(JNIEnv *env, jclass cls) {
+    size_t len = DEFAULT_BUF_LEN;
+    int fd = ASharedMemory_create("hardeningtest", len);
+    assert(fd >= 0);
+
+    void *addr = mmap(nullptr, len, PROT_EXEC, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    if (addr == MAP_FAILED) {
+        return get_errno();
+    }
+
+    munmap(addr, len);
+    return 0;
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/Utils.java
@@ -1,0 +1,93 @@
+package app.grapheneos.hardeningtest;
+
+import android.content.Context;
+import android.os.Process;
+import android.os.SELinux;
+import android.system.ErrnoException;
+import android.system.Os;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static android.system.OsConstants.EACCES;
+import static android.system.OsConstants.EPERM;
+import static android.system.OsConstants.O_CREAT;
+import static android.system.OsConstants.O_EXCL;
+import static android.system.OsConstants.O_RDONLY;
+import static android.system.OsConstants.O_RDWR;
+import static android.system.OsConstants.SEEK_SET;
+import static android.system.OsConstants.S_IRUSR;
+import static android.system.OsConstants.S_IWUSR;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class Utils {
+
+    public static void checkSELinuxContextAndFlags(String expectedPrefix, int targetSdk) {
+        String TAG = "SELinux";
+
+        String ctx = SELinux.getContext();
+        assertTrue("unexpected SELinux context: " + ctx + " ; expectedPrefix " + expectedPrefix,
+            ctx.startsWith(expectedPrefix));
+
+        int myPid = Process.myPid();
+
+        FileDescriptor fd = null;
+        try {
+            String path = "/proc/" + myPid + "/task/" + myPid + "/attr/selinux_flags";
+            fd = Os.open(path, O_RDWR, 0);
+            try (var s = new FileInputStream(fd)) {
+                var bytes = s.readAllBytes();
+                Log.d(TAG, "selinux_flags " + new String(bytes));
+            }
+            Os.lseek(fd, 0L, SEEK_SET);
+            try {
+                Os.write(fd, ByteBuffer.wrap("0".getBytes()));
+                // app should never be able to change selinux_flags
+                fail("write to selinux_flags should have failed");
+            } catch (ErrnoException e) {
+                int errno = e.errno;
+                if (errno != EPERM && errno != EACCES) {
+                    fail("selinux_flags should have failed with EPERM or EACCES, got "
+                        + Os.strerror(errno));
+                }
+            }
+        } catch (Throwable e) {
+            Log.d(TAG, "", e);
+            if (e instanceof AssertionError) {
+                throw (AssertionError) e;
+            }
+            throw new IllegalStateException(e);
+        } finally {
+            libcore.io.IoUtils.closeQuietly(fd);
+        }
+    }
+
+    public static int getFdForExecAppDataFileTest(Context ctx) throws IOException, ErrnoException {
+        File file = new File(ctx.getFilesDir(), "appdata_file_exe");
+        synchronized (Utils.class) {
+            file.delete();
+            Files.copy(Paths.get("/proc/self/exe"), file.toPath());
+            return Os.open(file.getPath(), O_RDONLY, 0).getInt$();
+        }
+    }
+
+    public static int getFdForExecmodTest(Context ctx) throws IOException, ErrnoException {
+        File file = new File(ctx.getFilesDir(), "execmod_exe");
+        synchronized (Utils.class) {
+            file.delete();
+            FileDescriptor fd = Os.open(file.getPath(), O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+            return fd.getInt$();
+        }
+    }
+
+    public static int getTargetSdk(Context ctx) {
+        return ctx.getApplicationInfo().targetSdkVersion;
+    }
+}

335015939hi/dumbos-platform_frameworks_base

tests/HardeningTest/HardeningTestApp/jni/hardening_test.cpp
@@ -1,0 +1,133 @@
+#include <cstdlib>
+
+#include <android/sharedmem.h>
+#undef NDEBUG // needed to enable assertions
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <jni.h>
+#include <linux/ashmem.h>
+#include <sys/mman.h>
+#include <sys/ptrace.h>
+#include <unistd.h>
+
+static jint get_errno() {
+    int r = errno;
+    assert(r > 0);
+    return (jint) r;
+}
+
+static constexpr size_t DEFAULT_BUF_LEN = 16 * 1024 + 10;
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execmem(JNIEnv *env, jclass cls) {
+    size_t len = DEFAULT_BUF_LEN;
+    void *addr = mmap(nullptr, len, PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (addr != MAP_FAILED) {
+        munmap(addr, len);
+        return 0;
+    }
+    return get_errno();
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execmod(JNIEnv *env, jclass cls, jint fd) {
+    char buf[DEFAULT_BUF_LEN];
+    assert((ssize_t) sizeof(buf) == write(fd, buf, sizeof(buf)));
+
+    void *addr = mmap(nullptr, sizeof(buf), PROT_WRITE, MAP_PRIVATE, fd, 0);
+    close(fd);
+    assert(addr != MAP_FAILED);
+
+    // modify the file mapping
+    (* ((char*) addr))++;
+
+    jint r = 0;
+    if (mprotect(addr, sizeof(buf), PROT_EXEC) != 0) {
+        r = get_errno();
+    }
+
+    munmap(addr, sizeof(buf));
+    return r;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_exec_1app_1data_1file(JNIEnv *env, jclass cls, jint fd) {
+    const char* argv[] = { "a", nullptr };
+    const char* envp[] = { "b", nullptr };
+
+    fexecve(fd, (char *const *) argv, (char *const *) envp);
+    close(fd);
+    return get_errno();
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_exec_1appdomain_1tmpfs(JNIEnv *env, jclass cls) {
+    int fd = memfd_create("hardeningtest_memfd", 0);
+    assert(fd >= 0);
+
+    size_t len = DEFAULT_BUF_LEN;
+
+    void *addr = mmap(nullptr, len, PROT_EXEC, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    if (addr == MAP_FAILED) {
+        return get_errno();
+    }
+
+    munmap(addr, len);
+    return 0;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_ptrace(JNIEnv *env, jclass cls) {
+    pid_t fork_res = fork();
+
+    if (fork_res == 0) {
+        // child
+        // not necessary and racy
+        // ptrace(PTRACE_TRACEME, 0, nullptr, 0);
+    } else {
+        assert(fork_res > 0);
+        pid_t child_pid = fork_res;
+        long r = ptrace(PTRACE_ATTACH, child_pid, nullptr, 0);
+        if (r == -1) {
+            return get_errno();
+        }
+        ptrace(PTRACE_DETACH, child_pid, nullptr, 0);
+    }
+    return 0;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execute_1ashmem(JNIEnv *env, jclass cls) {
+    // Based on ashmem_create_region() from system/core/libcutils/ashmem-dev.cpp
+    // Direct use of that function would return memfd instead of ashmem fd
+
+    int fd = open("/dev/ashmem", O_RDWR);
+    if (fd < 0) {
+        return get_errno();
+    }
+
+    size_t size = DEFAULT_BUF_LEN;
+
+    assert(ioctl(fd, ASHMEM_SET_SIZE, size) == 0);
+
+    void *addr = mmap(nullptr, size, PROT_EXEC, MAP_PRIVATE, fd, 0);
+    close(fd);
+    if (addr == MAP_FAILED) {
+        return get_errno();
+    }
+    munmap(addr, size);
+    return 0;
+}
+
+extern "C" jint Java_app_grapheneos_hardeningtest_MultiTests_execute_1ashmem_1libcutils(JNIEnv *env, jclass cls) {
+    size_t len = DEFAULT_BUF_LEN;
+    int fd = ASharedMemory_create("hardeningtest", len);
+    assert(fd >= 0);
+
+    void *addr = mmap(nullptr, len, PROT_EXEC, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    if (addr == MAP_FAILED) {
+        return get_errno();
+    }
+
+    munmap(addr, len);
+    return 0;
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/Utils.java
@@ -1,0 +1,93 @@
+package app.grapheneos.hardeningtest;
+
+import android.content.Context;
+import android.os.Process;
+import android.os.SELinux;
+import android.system.ErrnoException;
+import android.system.Os;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static android.system.OsConstants.EACCES;
+import static android.system.OsConstants.EPERM;
+import static android.system.OsConstants.O_CREAT;
+import static android.system.OsConstants.O_EXCL;
+import static android.system.OsConstants.O_RDONLY;
+import static android.system.OsConstants.O_RDWR;
+import static android.system.OsConstants.SEEK_SET;
+import static android.system.OsConstants.S_IRUSR;
+import static android.system.OsConstants.S_IWUSR;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class Utils {
+
+    public static void checkSELinuxContextAndFlags(String expectedPrefix, int targetSdk) {
+        String TAG = "SELinux";
+
+        String ctx = SELinux.getContext();
+        assertTrue("unexpected SELinux context: " + ctx + " ; expectedPrefix " + expectedPrefix,
+            ctx.startsWith(expectedPrefix));
+
+        int myPid = Process.myPid();
+
+        FileDescriptor fd = null;
+        try {
+            String path = "/proc/" + myPid + "/task/" + myPid + "/attr/grapheneos_flags";
+            fd = Os.open(path, O_RDWR, 0);
+            try (var s = new FileInputStream(fd)) {
+                var bytes = s.readAllBytes();
+                Log.d(TAG, "grapheneos_flags " + new String(bytes));
+            }
+            Os.lseek(fd, 0L, SEEK_SET);
+            try {
+                Os.write(fd, ByteBuffer.wrap("0".getBytes()));
+                // app should never be able to change grapheneos_flags
+                fail("write to grapheneos_flags should have failed");
+            } catch (ErrnoException e) {
+                int errno = e.errno;
+                if (errno != EPERM && errno != EACCES) {
+                    fail("grapheneos_flags should have failed with EPERM or EACCES, got "
+                        + Os.strerror(errno));
+                }
+            }
+        } catch (Throwable e) {
+            Log.d(TAG, "", e);
+            if (e instanceof AssertionError) {
+                throw (AssertionError) e;
+            }
+            throw new IllegalStateException(e);
+        } finally {
+            libcore.io.IoUtils.closeQuietly(fd);
+        }
+    }
+
+    public static int getFdForExecAppDataFileTest(Context ctx) throws IOException, ErrnoException {
+        File file = new File(ctx.getFilesDir(), "appdata_file_exe");
+        synchronized (Utils.class) {
+            file.delete();
+            Files.copy(Paths.get("/proc/self/exe"), file.toPath());
+            return Os.open(file.getPath(), O_RDONLY, 0).getInt$();
+        }
+    }
+
+    public static int getFdForExecmodTest(Context ctx) throws IOException, ErrnoException {
+        File file = new File(ctx.getFilesDir(), "execmod_exe");
+        synchronized (Utils.class) {
+            file.delete();
+            FileDescriptor fd = Os.open(file.getPath(), O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+            return fd.getInt$();
+        }
+    }
+
+    public static int getTargetSdk(Context ctx) {
+        return ctx.getApplicationInfo().targetSdkVersion;
+    }
+}